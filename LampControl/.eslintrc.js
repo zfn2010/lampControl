ecbddf492247ab48018c0121bffd3528147e3e5d
zfn2010 <1527348287@qq.com>
2017-04-25 18:42:32 +0800

    统一Android和ios入口

---
+++ LampControl/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "react/prefer-stateless-function": [0, { "ignorePureComponents": true }]
+    }
+};
--- LampControl/index.android.js
+++ LampControl/index.android.js
@@ -4,50 +4,17 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
+import React, {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View
+  Component,
 } from 'react-native';
 
-export default class LampControl extends Component {
+import Main from './js/page/main';
+
+class LampControl extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+    return (<Main />);
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('LampControl', () => LampControl);
